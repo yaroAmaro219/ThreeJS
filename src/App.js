133dbfac2f8f63b6d8d406f7c3edaf16a49059ed
Artemie Amari <yaroamari@gmail.com>
2020-07-19 08:18:09 -0400

    Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Canvas, useFrame } from 'react-three-fiber';
+import { softShadows, MeshWobbleMaterial, OrbitControls } from 'drei';
+import Header from './components/Header'
+import {useSpring, a} from 'react-spring/three'
+//import './App.css';
 
-function App() {
+softShadows();
+
+const SpinningMesh = ({ position, color, speed, args }) => {
+  const mesh = useRef();
+  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01))
+  const [expand, setExpand] = useState(false);
+  const props = useSpring({
+    scale: expand ? [1.4, 1.4, 1.4] : [1, 1, 1],
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <a.mesh
+      position={position}
+      ref={mesh}
+      onClick={() => setExpand(!expand)}
+      scale={props.scale}
+      castShadow>
+      <boxBufferGeometry
+        attach='geometry' 
+        args={args} />
+      <MeshWoblleMaterial
+        color={color}
+        speed={speed}
+        attach='material'
+        factor={0.6}
+        />
+    </a.mesh>
+  )
+}
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Canvas
+        class="canvas"
+        colorManagement
+        shadowMap
+        camera={{ position: [-5, 2, 10], fov: 10 }}>
+        <ambientLight 
+          intensity={0.3} />
+        <directionalLight
+          castShadow
+          position={[0, 10, 0]}
+          intensity={1.5}
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+          shadow-camera-far={50}
+          shadow-camera-left={-10}
+          shadow-camera-right={10}
+          shadow-camera-top={10}
+          shadow-camera-bottom={-10}
+        />
+        <pointLight 
+          position={[-10, 0, -20]} 
+          intensity={0.5}
+        />
+        <pointLight
+          position={[0, -10, 0]}
+          intensity={1.5}
+        />
+        <group>
+          <mesh
+            rotation={[-Math.PI / 2, 0, 0]}
+            position={[0, -3, 0]}
+            receiveShadow>
+            <planeBufferGeometry attach='geometry' args={[100, 100]} />
+            <meshStandardMaterial attach='material' color={"yellow"} />
+          </mesh>
+          <SpinningMesh
+            position={[0, 1, 0]}
+            color='lightblue'
+            args={[3, 2, 1]}
+            speed={2}
+          />
+          <SpinningMesh 
+            position={[-2, 1, -5]} 
+            color='pink'
+            speed={6}
+          />
+          <SpinningMesh
+            position={[5, 1, -2]}
+            color='pink'
+            speed={6}
+            />
+        </group>
+        <OrbitContols />
+        </Canvas>
+      </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,105 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom'
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Canvas } from 'react-three-fiber'
+import { useTransition, useSpring, a } from 'react-spring/three'
+import { svgs, colors, deg, doubleSide } from './resources/helper'
+import { BrowserRouter as Router } from 'react-router-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Shape({ shape, rotation, position, color, opacity, index }) {
+  return (
+    <a.mesh 
+      rotation={rotation} 
+      position={position.interpolate((x, y, z) => [x, y, z + -index * 50])}>
+      <a.meshPhongMaterial
+        attach="material"
+        color={color}
+        opacity={opacity}
+        side={doubleSide}
+        depthWrite={false}
+        transparent />
+      <shapeBufferGeometry 
+        attach="geometry"
+        args={[shape]} 
+        />
+      </a.mesh>
+  )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function Scene() {
+  const [page, setPage] = useState(0)
+  const [shapes, setShapes] = useState([])
+  useEffect(() => void setInterval(() => setPage(i => (i + 1) % svgs.length), 4000), [])
+  useEffect(() => void svgs[page].then(setShapes), [page])
+  const { color } = useSpring({ color: colors[page] })
+  const transitions = useTransition(shapes, item => item.shape.uuid, {
+    from: { rotation: [-0.2, 0.9, 0], position: [0, 50, -200], opacity: 0 },
+    enter: { rotation: [0, 0, 0], position: [0, 0, 0], opacity: 1 },
+    leave: { rotation: [0.2, -0.9, 0], position: [0, -400, 200], opacity: 0 },
+    config: { mass: 30, tension: 800, friction: 190, precision: 0.0001 },
+    ...{ order: ['leave', 'enter', 'update'], trail: 15, lazy: true, unique: true, reset: true }
+  })
+  return (
+    <>
+      <mesh
+        scale={[20000, 20000, 1]}
+        rotation={[0, deg(-20), 0]}>
+        <planeGeometry 
+          attach="geometry" 
+          args={[1, 1]} />
+        <a.meshPhongMaterial 
+          attach="material" 
+          color={color}
+          depthTest={false}/>
+      </mesh>
+      <group
+        position={[-1200, 300, page]} rotation={[0, deg(-180), deg(180)]}>
+        {transitions.map(({ item, key, props }) => (
+          <Shape 
+            key={key}
+            {...item}
+            {...props}
+          />
+        ))}
+        </group>
+      </>
+  )
+}
+
+function App() {
+  return (
+    <div class="main">
+      <Canvas 
+        invalidateFrameloop
+        camera={{
+          fov: 100,
+          position: [10, 10, 2700],
+          rotation: [0, deg(-20), deg(270)],
+          near: .01, far: 20000
+        }}>
+        <ambientLight intensity={0.5} />
+        <spotLight intensity={0.5} position={[300, 300, 3000]} />
+        <Scene />
+      </Canvas>
+      {/* <a href="https://github.com/yaroAmaro219?tab=repositories" target="_blank" rel="noopener noreferrer" class="top-left" children="Github" /> */}
+      <a href="https://soundcloud.com/artemie-amari/tracks" target="_blank" rel="noopener noreferrer" class="top-left" children="Soundcloud" />
+      <a href="https://www.linkedin.com/in/artemie-amari-337b78117/" target="_blank" rel="noopener noreferrer" class="top-right" children="LinkedIn" />
+      <a href="http://www.artemieamari.com/work" target="_blank" rel="noopener noreferrer" class="bottom-left" children="Portfolio" />
+      <a href="https://www.instagram.com/tina.henschel/" target="_blank" rel="noopener noreferrer" class="bottom-right" children="Illustrations @ Tina Henschel" />
+      <span class="header">
+        Artemie Amari
+         
+      </span>
+      
+        <Link to='/' class="menu1">About</Link>
+        <Link to='/' class="menu2">Portfolio</Link>
+        <Link to='/' class="menu3">Contact</Link>
+        
+      
+    </div>
+  )
+}
+
+ReactDOM.render(<Router><App /></Router>,document.getElementById('root'));
+
+
